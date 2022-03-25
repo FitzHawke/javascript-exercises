a77ccab30cc56159810b967e6a3644c6bb709212
Fitz Hawke <will@fitzhawk.com>
2022-03-25 18:40:12 -0400

    solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let args = Array.from(arguments);
+    let newArray = args[0];
 
+    for (let i = 1; i < args.length; i++) {
+        for (let j = 0; j < newArray.length; j++) {
+            if (args[i] === newArray[j]) {
+                newArray.splice(j, 1);
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
