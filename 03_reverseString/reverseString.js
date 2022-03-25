50618f71ede1b4de5b1573409b283b95ada41af6
Fitz Hawke <will@fitzhawk.com>
2022-03-25 17:41:08 -0400

    solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    const chars = string.split("");
+    let newString = "";
 
+    for (i = 0; i < string.length; i++) {
+        let letter = chars.pop();
+        newString += letter;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
