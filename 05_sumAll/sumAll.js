5e95ee012f6ae329b0ff45dbc793c553be4c262c
Fitz Hawke <will@fitzhawk.com>
2022-03-25 19:39:23 -0400

    solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+function doCount(one, two) {
+    let newNum = 0;
+    for (let i = one; i <= two; i++) {
+        newNum += i;
+    }
+    return newNum;
+}
 
+const sumAll = function (numOne, numTwo) {
+    if (!Number.isInteger(numOne) || numOne < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(numTwo) || numTwo < 0) {
+        return "ERROR";
+    }
+
+    if (numOne > numTwo) {
+        return doCount(numTwo, numOne);
+    } else {
+        return doCount(numOne, numTwo);
+    }
 };
 
 // Do not edit below this line
